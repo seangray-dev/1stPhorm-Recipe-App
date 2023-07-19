cf40f26e9bdafa1319df596aea0046f9dcd811b9
Sean Gray <seangray.dev@gmail.com>
2023-07-18 20:34:09 -0400

    delete template tab pages + add loader, dashboard, nutrition, recipe pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,24 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'loader',
+    loadComponent: () =>
+      import('./loader/loader.page').then((m) => m.LoaderPage),
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./dashboard/dashboard.page').then((m) => m.DashboardPage),
+  },
+  {
+    path: 'nutrition',
+    loadComponent: () =>
+      import('./nutrition/nutrition.page').then((m) => m.NutritionPage),
+  },
+  {
+    path: 'recipes',
+    loadComponent: () =>
+      import('./recipes/recipes.page').then((m) => m.RecipesPage),
+  },
 ];
--- src/app/tabs/tabs.routes.ts
+++ src/app/tabs/tabs.routes.ts
@@ -7,30 +7,30 @@ export const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
+        path: 'dashboard',
         loadComponent: () =>
-          import('../tab1/tab1.page').then((m) => m.Tab1Page),
+          import('../dashboard/dashboard.page').then((m) => m.DashboardPage),
       },
       {
-        path: 'tab2',
+        path: 'nutrition',
         loadComponent: () =>
-          import('../tab2/tab2.page').then((m) => m.Tab2Page),
+          import('../nutrition/nutrition.page').then((m) => m.NutritionPage),
       },
       {
-        path: 'tab3',
+        path: 'recipes',
         loadComponent: () =>
-          import('../tab3/tab3.page').then((m) => m.Tab3Page),
+          import('../recipes/recipes.page').then((m) => m.RecipesPage),
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/dashboard',
         pathMatch: 'full',
       },
     ],
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/dashboard',
     pathMatch: 'full',
   },
 ];
